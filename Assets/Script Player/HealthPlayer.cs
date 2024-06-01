47047e474ac39e9eb42b8135f1f121f499c5394d
creeperTanken <stasbagger@gmail.com>
2024-06-01 14:44:18 +0300

    Update 0.2

--- Assets/Script Player/HealthPlayer.cs
+++ Assets/Script Player/HealthPlayer.cs
@@ -1,28 +1,75 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.UI;
 using UnityEngine;
 
 public class HealthPlayer : MonoBehaviour
 {
-    public float health;
-    public float maxHealth;
+    private float health;
+    private float maxHealth = 3;
     public GameObject healthValue;
+    public Image[] healths = new Image[3];
 
-    public void TakeHit(float damage)
+    private void Start()
     {
-        health -= damage;
+        health = 3;
+    }
+
+
+    public void TakeHit()
+    {
+        health -= 1;
+        HealthScore(health);
 
         if (health <= 0)
         {
             Destroy(gameObject);
         }
+
+
     }
 
-    public void SetHealth(int bonusHealth)
+    public void SetHealth()
     {
-        health += bonusHealth;
+        health += 1;
+        HealthScore(health);
 
         if (health >= maxHealth)
             health = maxHealth;
+    }
+
+    private void HealthScore(float health)
+    {
+        switch (health)
+        {
+            case 3:
+                {
+                    healths[0].enabled = true;
+                    healths[1].enabled = true;
+                    healths[2].enabled = true;
+                    break;
+                }
+            case 2:
+                {
+                    healths[0].enabled = true;
+                    healths[1].enabled = true;
+                    healths[2].enabled = false;
+                    break;
+                }
+            case 1:
+                {
+                    healths[0].enabled = true;
+                    healths[1].enabled = false;
+                    healths[2].enabled = false;
+                    break;
+                }
+            case 0:
+                {
+                    healths[0].enabled = false;
+                    healths[1].enabled = false;
+                    healths[2].enabled = false;
+                    break;
+                }
+        }
     }
 }
--- Assets/Script Player/MovePlayer.cs
+++ Assets/Script Player/MovePlayer.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Playables;
 using UnityEngine;
 
 public class MovePlayer : MonoBehaviour
 {
-    public float speed, verticJump, horizJump;
+    public float speed, verticJump, horizJump, damageHJump, damageVJump;
+    public float secondInvicible;
+    private bool invicible = false;
     public Camera cam;
     private Rigidbody2D _rb;
 
@@ -25,5 +28,22 @@ public class MovePlayer : MonoBehaviour
             Debug.Log("Trampoline");
             _rb.AddForce(new Vector2(horizJump * 1000, verticJump * 1000) * Time.fixedDeltaTime);
         }
+
+        if (invicible == false)
+            if (collision.gameObject.tag == "Spikes")
+            {
+                Debug.Log("Spikes");
+                HealthPlayer health = gameObject.GetComponent<HealthPlayer>();
+                _rb.AddForce(new Vector2(damageHJump * 1000, damageVJump * 1000) * Time.fixedDeltaTime);
+                health.TakeHit();
+                StartCoroutine(Invicible(secondInvicible));
+            }
+    }
+
+    private IEnumerator Invicible(float second)
+    {
+        yield return new WaitForSeconds(second);
+        invicible = true;
+
     }
 }
