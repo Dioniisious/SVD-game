ce97d63bf6d3be07cdc815eff02a2d36284db093
Денис Гасников <79269975577@ya.ru>
2024-06-01 15:04:44 +0300

    Добавление новых картинок

--- Assets/Scripts/SomeScrr.cs
+++ Assets/Scripts/SomeScrr.cs
@@ -7,7 +7,7 @@ public class SomeScrr : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Let's play!");
     }
 
     // Update is called once per frame
